4b22ca0a905533161fe15c94eedb578c4a6f2d31
Miguel <michelnd@hotmail.com>
2023-10-21 17:39:32 +0100

    SplashView Terminado

--- DBHeroes/UI/Login/LoginViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  LoginViewModel.swift
-//  DBHeroes
-//
-//  Created by Miguel Nantón Díaz on 21/10/23.
-//
-
-import Foundation
--- DBHeroes/UI/Main/MainViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MainViewController.swift
-//  DBHeroes
-//
-//  Created by Miguel Nantón Díaz on 21/10/23.
-//
-
-import Foundation
